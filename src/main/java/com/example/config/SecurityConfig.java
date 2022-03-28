650f0b0ec3041828c8f5866643146a2067503380
akira <2015ultra339@gmail.com>
2022-03-28 17:09:14 +0900

    独自ユーザークラスの作成

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.example.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -20,6 +22,10 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    @Qualifier("UserDetailsServiceImpl")
+    private UserDetailsService userDetailsService;
+
     //パスワードエンコーダーのBean定義
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,29 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
-    // ユーザーIDとパスワードを取得するSQL文
-    private static final String USER_SQL = "SELECT"
-            + "    user_id,"
-            + "    password,"
-            + "    enabled"
-            + " FROM"
-            + "    m_user"
-            + " WHERE"
-            + "    user_id = ?";
-
-    // ユーザーのロールを取得するSQL文
-    private static final String ROLE_SQL = "SELECT"
-            + "    m_user.user_id,"
-            + "    role.role_name"
-            + " FROM"
-            + "    m_user INNER JOIN t_user_role AS user_role"
-            + " ON"
-            + "    m_user.user_id = user_role.user_id"
-            + "    INNER JOIN m_role AS role"
-            + " ON"
-            + "    user_role.role_id = role.role_id"
-            + " WHERE"
-            + "    m_user.user_id = ?";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -86,11 +69,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         // ログイン処理時のユーザー情報を、DBから取得する
-        auth.jdbcAuthentication()
-            .dataSource(dataSource)
-            .usersByUsernameQuery(USER_SQL) //ユーザーの取得
-            .authoritiesByUsernameQuery(ROLE_SQL) //ロールの取得
-            .passwordEncoder(passwordEncoder()); //パスワードの復号
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder());
 
     }
 }
